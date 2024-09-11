d82a8db8456c0ea2aff4cde57ccaa172ccdd843c
Leonid Poluiakhtov <len4ik-0123@yandex.ru>
2024-09-12 01:15:26 +0400

    9 модуль. Задание #2. Сравнение объектов

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,8 +9,8 @@ public class Main {
         int count = 0;
         while (true) {
             System.out.println("Укажите путь к файлу:");
-            String path = new Scanner(System.in).nextLine();
-            File file = new File(path);
+            String pathFile = new Scanner(System.in).nextLine();
+            File file = new File(pathFile);
             boolean fileExists = file.exists();
             boolean isDirectory = file.isDirectory();
             if (!fileExists) {
@@ -22,29 +24,62 @@ public class Main {
             count++;
             System.out.println("Это файл номер " + count);
 
-            //Обработка исключений
             try {
-                FileReader fileReader = new FileReader(path);
+                FileReader fileReader = new FileReader(pathFile);
                 BufferedReader reader = new BufferedReader(fileReader);
+                String logPattern =
+                        "(\\d+\\.\\d+\\.\\d+\\.\\d+)" + // IP-адрес
+                                " (\\S*) (\\S*) " + // Два пропущенных свойства
+                                "\\[(.+?)\\] " + // Дата и время запроса
+                                "\"(\\w+ .+? HTTP/\\d\\.\\d\") " + // Метод и путь запроса
+                                "(\\d{3}) " + // Код HTTP-ответа
+                                "(\\d+) " + // Размер отданных данных
+                                "\"([^\"]*)\" " + // Referer
+                                "\"([^\"]*)\""; // User-Agent
+                Pattern pattern = Pattern.compile(logPattern);
                 String line;
                 int countLines = 0;
-                int minLength = 2147483647; //присваиваем максимальное значение в диапазоне int
-                int maxLength = 0;
+                int countGoogle = 0;
+                int countYandex = 0;
                 while ((line = reader.readLine()) != null) {
                     countLines++;
                     int length = line.length();
                     if (length > 1024) {
-//                        throw new RuntimeException("В файле строка №" + countLines + " длиной более 1024 символов!"); //1 способ. Исключение через объект класса RuntimeException.
-                        throw new LengthExceededException("В файле строка №" + countLines + " длиной более 1024 символов!"); //2 способ. Создан отдельный класс исключения.
+                        throw new LengthExceededException("В файле строка №" + countLines + " длиной более 1024 символов!");
                     }
-                    if (length < minLength)
-                        minLength = length;
-                    if (length > maxLength)
-                        maxLength = length;
+                    Matcher matcher = pattern.matcher(line);
+                    if (matcher.find()) {
+                        String ip = matcher.group(1);
+                        String prop1 = matcher.group(2);
+                        String prop2 = matcher.group(3);
+                        String dateTime = matcher.group(4);
+                        String path = matcher.group(5);
+                        String httpCode = matcher.group(6);
+                        String dataSize = matcher.group(7);
+                        String referer = matcher.group(8);
+                        String userAgent = matcher.group(9); //В приложении выделяется фрагмент User-Agent
+
+                        //userAgent
+                        String googleBotPattern = ".Googlebot.";
+                        String yandexBotPattern = ".YandexBot.";
+                        Pattern googlePattern = Pattern.compile(googleBotPattern);
+                        Pattern yandexPattern = Pattern.compile(yandexBotPattern);
+                        Matcher googleMatcher = googlePattern.matcher(userAgent); //Из фрагмента User-Agent успешно отделяется часть, которая в случае запросов от соответствующих ботов равна строкам "YandexBot" или "GoogleBot"
+                        Matcher yandexMatcher = yandexPattern.matcher(userAgent);
+                        if (googleMatcher.find())
+                            countGoogle++; //Программа корректно подсчитывает долю запросов от GoogleBot
+                        if (yandexMatcher.find())
+                            countYandex++; //Программа корректно подсчитывает долю запросов от YandexBot
+                    } else
+                        System.out.println("Не найдено совпадений в строке: " + line);
                 }
                 System.out.println("Общее количество строк в файле: " + countLines);
-                System.out.println("Длина самой длинной строки в файле: " + maxLength);
-                System.out.println("Длина самой короткой строки в файле: " + minLength);
+                System.out.println("Количество строк с Googlebot: " + countGoogle);
+                System.out.println("Количество строк с YandexBot: " + countYandex);
+                if (countLines != 0) {
+                    System.out.println("Доля запросов от GoogleBot: " + (double) countGoogle / (double) countLines);
+                    System.out.println("Доля запросов от YandexBot: " + (double) countYandex / (double) countLines);
+                } else throw new IllegalArgumentException("Значение элемента countLines = 0, деление на 0 невозможно!");
                 reader.close();
                 fileReader.close();
             } catch (FileNotFoundException ex) {
@@ -55,18 +90,5 @@ public class Main {
                 System.out.println("Ошибка чтения файла: " + ex.getMessage());
             }
         }
-        //Задание по теме ""Синтаксис языка" и “Базовые типы данных”"
-//        System.out.println("Введите первое число: ");
-//        int firstNumber = new Scanner(System.in).nextInt();
-//        System.out.println("Введите второе число: ");
-//        int secondNumber = new Scanner(System.in).nextInt();
-//        int sum = firstNumber + secondNumber;
-//        int diff = firstNumber - secondNumber;
-//        int product = firstNumber * secondNumber;
-//        double quotient = (double) firstNumber / secondNumber;
-//        System.out.println("Сумма: " + sum);
-//        System.out.println("Разность: " + diff);
-//        System.out.println("Произведение: " + product);
-//        System.out.println("Частное: " + quotient);
     }
 }
